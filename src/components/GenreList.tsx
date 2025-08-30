2e734e09c57822aa86a05bbc3752f68dbca4d272
KIM YUEN <kim_man@live.com>
2025-08-30 13:34:11 +1000

    show a spinner while fetching the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,13 @@
 import useFetchGenres from '@/hooks/useFetchGenres';
 import getCroppedImageURL from '@/services/img-url';
-import { Text, Image, HStack, List, ListItem } from '@chakra-ui/react';
+import { Text, Image, HStack, List, ListItem, Spinner } from '@chakra-ui/react';
 
 const GenreList = () => {
   const { data, error, loading } = useFetchGenres();
   return (
     <List>
-      {error && <Text>{error}</Text>}
+      {error && null}
+      {loading && <Spinner />}
       {data.map((genre) => (
         <ListItem key={genre.id}>
           <HStack paddingY="5px">
